fc33f696858aefc5960961fc10dfe34a6ff2d6ea
Mariama Diallo <diallomt193@yahoo.com>
2022-08-21 08:31:14 -0400

    created tables and connected to the database

---
+++ Level 1/schema.sql
@@ -0,0 +1,18 @@
+-- CREATE TABLE product_review (
+--   customer_id INTEGER,
+--   review_id VARCHAR,
+--   star_rating INTEGER,
+--   helpful_votes INTEGER,
+--   total_votes INTEGER
+-- );
+
+-- CREATE TABLE gift_card_review (
+--   marketplace VARCHAR,
+--   product_id VARCHAR,
+--   star_rating INTEGER,
+--   total_votes INTEGER,
+--   review_date VARCHAR	
+-- );
+
+SELECT * FROM gift_card_review;
+-- SELECT * FROM product_review;
